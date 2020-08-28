9d4f20d43d08c9a4b4fcbff3d651fe65efee4ec6
Tristan Miller <tem83@nau.edu>
2020-08-27 18:12:28 -0700

    Started work on adding text labels to the peaks

--- js/View/Track/MultiXYPlot.js
+++ js/View/Track/MultiXYPlot.js
@@ -8,36 +8,94 @@ function (
     Util,
     XYPlot
 ) {
-    return declare([ XYPlot], {
-        _defaultConfig: function () {
-            return Util.deepUpdate(dojo.clone(this.inherited(arguments)), {
-                onHighlightClick: function (feature, track) {
-                    var features = JSON.parse(localStorage.getItem('ipaFeatures'));
-                    var states = ['unknown', 'peak', 'nopeak'];
-                    features.forEach(f => {
-                        if (f.ref === feature.get('ref') &&
-                            f.start === feature.get('start') &&
-                            f.end === feature.get('end')
-                        ) {
-                            if (f[track.name]) {
-                                f[track.name] = (f[track.name] + 1) % states.length;
+    return declare([ XYPlot],
+        {
+            _defaultConfig: function () {
+                return Util.deepUpdate(dojo.clone(this.inherited(arguments)),
+                    {
+                        showLabels: true,
+                        onHighlightClick: function (feature, track) {
+                            // grab known labels and
+                            let features = JSON.parse(localStorage.getItem('ipaFeatures'));
+                            // eslint-disable-next-line radix
+                            const highlightFlag = parseInt(localStorage.getItem('highlightFlag'));
+                            // uses highlightFlag to determine if editing or removing
+                            if (highlightFlag === 1) {
+                                // this is wear we define the types of labels, to create a new kind of
+                                const states = ['unknown', 'peak', 'noPeak', 'peakStart', 'peakEnd'];
+                                // label add to this list and the two down below for determining the color
+                                // loops through known labels for the label clicked
+                                features.forEach(f => {
+                                    if (f.ref === feature.get('ref') &&
+                               f.start === feature.get('start') &&
+                               f.end === feature.get('end')) {
+                                        //TODO: Fix this
+                                        if (f[track.name]) {
+                                            // increments the track type
+                                            f[track.name] = states[(f[track.name] + 1) % states.length];
+                                        } else {
+                                            // if the track has no type set to "Peak"
+                                            f[track.name] = 'unknown';
+                                        }
+                                    }
+                                });
                             } else {
-                                f[track.name] = 1;
+                                // loop through labels removing clicked one
+                                // eslint-disable-next-line consistent-return
+                                features = features.filter(function (f) {
+                                    if (f.start !== feature.get('start')) {
+                                        return f;
+                                    }
+                                });
+                                // json of information of removed label
+                                var removeJSON = {
+                                    'name': track.name,
+                                    'ref': feature.get('ref'),
+                                    'start': feature.get('start'),
+                                    'end': feature.get('end')
+                                };
+                                // eslint-disable-next-line no-undef
+                                sendPost('remove', removeJSON);
+                            }
+                            // redraw to update model
+                            track.browser.view.redrawTracks()
+                            localStorage.setItem('ipaFeatures', JSON.stringify(features));
+                        },
+
+                        highlightColor: function (feature, track) {
+                            // determins the color of the see through part of the label
+                            // to add new type of label add type to this list
+                            const states = {
+                                'unknown': 'rgba(100,100,100,.4)',
+                                'peak': '#0f05',
+                                'noPeak': '#ff05',
+                                'peakStart': 'rgba(255,0,0,0.4)',
+                                'peakEnd': 'rgba(255,150,0,0.4)'
+                            };
+                            return states[feature.data[track.name] || 'unknown'];
+                        },
+
+                        indicatorColor: function (feature, track) {
+                            // determins the color of the bar at the bottom of the label
+                            // to add new type of label add type to this list
+                            const states = {
+                                'unknown': '#f00',
+                                'peak': '#0f0',
+                                'noPeak': '#ff0',
+                                'peakStart': '#d3034f',
+                                'peakEnd': '#d30303'};
+                            return states[feature.data[track.name] || 'unknown'];
+                        },
+
+                        style:
+                        {
+                            label: function( feature, track )
+                            {
+                                const states = ['unknown', 'peak', 'noPeak', 'peakStart', 'peakEnd'];
+                                return states[feature.data[track.name] || 'unknown'];
                             }
                         }
                     });
-                    localStorage.setItem('ipaFeatures', JSON.stringify(features));
-                    track.redraw();
-                },
-                highlightColor: function (feature, track) {
-                    var states = {0: '#f005', 1: '#0f05', 2: '#ff05'};
-                    return states[feature.data[track.name] || 0];
-                },
-                indicatorColor: function (feature, track) {
-                    var states = {0: '#f00', 1: '#0f0', 2: '#ff0'};
-                    return states[feature.data[track.name] || 0];
-                }
-            });
-        }
-    });
+            }
+        });
 });
